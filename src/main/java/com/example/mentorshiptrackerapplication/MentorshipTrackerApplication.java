5e86e6dc075369c975e03ac6c1b1941085c1f89b
Robert-WilliamEvans <robert-william.evans@amalitech.com>
2023-06-25 21:32:53 +0000

    Adding the er diagram for the application

---
+++ src/main/java/com/example/mentorshiptrackerapplication/MentorshipTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentorshiptrackerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshipTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorshipTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentorshiptrackerapplication/MentorshipTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentorshiptrackerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshipTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
